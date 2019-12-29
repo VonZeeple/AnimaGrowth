d57738e4aadc31f72f9d1f56c54291bdbe3f8c6a
VonZeeple <vonzeeple@gmail.com>
2019-12-29 15:57:12 -0500

    -Added friendly names to configs -Added mcmod.info file Signed-off-by: VonZeeple <vonzeeple@gmail.com>

--- src/main/java/vonzeeple/animagrowth/ModConfig.java
+++ src/main/java/vonzeeple/animagrowth/ModConfig.java
@@ -3,10 +3,13 @@ import net.minecraftforge.common.config.Config;
 
 @Config(modid = AnimaGrowth.MODID)
 public class ModConfig {
+    @Config.Name("Pancheon")
     @Config.Comment("Allow pancheon to work with Animania milk")
     public static boolean config_pancheon = true;
+    @Config.Name("Honey")
     @Config.Comment("Allow fermentation of Animania honey")
     public static boolean config_honey = true;
+    @Config.Name("Kumi")
     @Config.Comment("Allow fermentation of Animania milk")
     public static boolean config_milk = true;
 }
